725b7b3849835b6b6f790d68aa106d9dfff0685a
Calvin Leung <kalvin807@gmail.com>
2020-08-18 22:20:45 +0800

    Increase the timeout for fetching things

--- web/src/api.js
+++ web/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: `https://${process.env.VUE_APP_BACKEND_URI}`,
-  timeout: 1000,
+  timeout: 10000,
 });
 
 export const get = async (dir, fileName) => {
