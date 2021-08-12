aa6bf444ab0fea4288821978c49dc93b2bd1ec09
Calvin Leung <kalvin807@gmail.com>
2021-08-12 18:50:12 +0800

    swap to echo and code clean up

--- application.go
+++ application.go
@@ -1,73 +1,45 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
-	"github.com/julienschmidt/httprouter"
 	"github.com/kalvin807/gbf-raid-finder/internal/clients"
 	"github.com/kalvin807/gbf-raid-finder/internal/fetcher"
 	"github.com/kalvin807/gbf-raid-finder/internal/router"
-	"github.com/rs/cors"
-	"github.com/urfave/negroni"
-)
 
-func setupLog() {
-	if _, err := os.Stat("log"); os.IsNotExist(err) {
-		errDir := os.MkdirAll("log", 0755)
-		if errDir != nil {
-			log.Fatal(err)
-		}
-	}
-	file, err := os.OpenFile("./log/run.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	mw := io.MultiWriter(os.Stdout, file)
-	log.SetOutput(mw)
-	log.Println("Go start farm in gbf!")
-}
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
 	setupLog()
 
-	dotenvErr := godotenv.Load(".env")
-	if dotenvErr != nil {
-		log.Println("Error loading .env file")
-	}
+	cfg := loadConfig()
 
-	port := os.Getenv("PORT")
+	client := fetcher.MakeTwitterClient(
+		cfg.TwitterApiKey,
+		cfg.TwitterApiSecert,
+		cfg.TwitterAccessKey,
+		cfg.TwitterAccessSecert,
+	)
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	apiKey, apiSecert := os.Getenv("TWITTER_API_KEY"), os.Getenv("TWITTER_API_SECRET")
-	accessKey, accessSecert := os.Getenv("TWITTER_ACCESS_KEY"), os.Getenv("TWITTER_ACCESS_SECRET")
-
-	if apiKey == "" || apiSecert == "" || accessKey == "" || accessSecert == "" {
-		log.Fatal("Missing twitter credentials in .env)")
-	}
-
-	client := fetcher.MakeTwitterClient(apiKey, apiSecert, accessKey, accessSecert)
 	hub := clients.NewHub(client)
 
 	go hub.Run()
 
-	c := cors.Default()
+	/**
+	 Router
+	**/
 
-	r := httprouter.New()
-	router.SetUpRoute(r, hub)
+	e := echo.New()
 
-	n := negroni.Classic()
-	n.Use(c)
-	n.UseHandler(r)
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Decompress())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.CORS())
 
-	err := http.ListenAndServe(":"+port, n)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	router.SetUpRoute(e, hub)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":" + cfg.Port))
 }
